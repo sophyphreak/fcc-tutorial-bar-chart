b94524a7d2039b9eb9f04fff56475b5092d69f50
Andrew <arhorn@smcm.edu>
2019-02-06 02:53:47 +0700

    Added faux dom for d3

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+import {withFauxDOM} from 'react-faux-dom'
+
+class Line extends React.Component {
+
+    componentDidMount() {
+       const faux = this.props.connectFauxDOM('div', 'chart');
+
+       // D3 Code to create the chart
+       // using faux as container
+
+       const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+       
+       const w = 500;
+       const h = 200;
+       
+       const svg = d3
+         .select(faux)
+         .append('svg')
+         .attr('width', w)
+         .attr('height', h);
+       
+       svg
+         .selectAll('rect')
+         .data(dataset)
+         .enter()
+         .append('rect')
+         .attr('x', (d, i) => i * 30)
+         .attr('y', (d, i) => h - 3 * d)
+         .attr('width', 25)
+         .attr('height', (d, i) => d * 3)
+         .attr('fill', 'navy')
+         .attr('class', 'bar')
+         .append('title')
+         .text(d => d);
+       
+       svg
+         .selectAll('text')
+         .data(dataset)
+         .enter()
+         .append('text')
+         .text(d => d)
+         .attr('x', (d, i) => i * 30)
+         .attr('y', (d, i) => h - (d * 3 + 3));
+    }
+
+    render() {
+      return (
+         <div className="line-container">
+             {this.props.chart}
+          </div>
+
+      )
+      }
 }
 
-export default App;
+export default withFauxDOM(Line);
